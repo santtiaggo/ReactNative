d0e35dfe9232427a5b680ced206d2b3384f6df26
Santiago Astrada <64703866+santtiaggo@users.noreply.github.com>
2020-11-30 13:10:29 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,38 @@ import { StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {DrawerNavigator} from './Navigators/DrawerNavigator.js';
 
+
+import LoginScreen from "./Screens/LoginScreen";
+import MainScreen from "./Screens/MainScreen";
+
+
 export default class App extends Component {
+
+  /*constructor(props){
+    super(props);
+    this.state = {isLoggedIn:false}
+}
+
+setLogin(){
+    this.setState({isLoggedIn:true});
+}
+
+setLogout(){
+    this.setState({isLoggedIn:false});
+}
+
+render(){
+
+    let screen;
+    if(this.state.isLoggedIn){
+        screen = <MainScreen onLogout={() => this.setLogout()}/>
+    } else {
+        screen = <LoginScreen onLogin={() => this.setLogin()}/>
+    }
+
+    return screen;
+
+}*/
 
   
   render(){
