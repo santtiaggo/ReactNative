3d54578bfe7fd6b9728384ac51bf9ed95e960dc5
Santiago Astrada <64703866+santtiaggo@users.noreply.github.com>
2020-12-03 11:47:09 -0300

    Update HomeScreen.js

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -114,16 +114,3 @@ const styles = StyleSheet.create({
       marginTop: 100,
     }
   });
-
- 
-
-
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#f0f',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
